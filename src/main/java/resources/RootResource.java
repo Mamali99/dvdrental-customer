1d718dae655a39fcba5b01b704a68902f6ef639b
Mohammad Ghalandari <m.ghalandari@ostfalia.de>
2023-11-07 13:51:45 +0100

    getRoot gibt richtige Pfaden zurückf

--- src/main/java/resources/RootResource.java
+++ src/main/java/resources/RootResource.java
@@ -3,15 +3,30 @@ package resources;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriInfo;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Path("/")
 public class RootResource {
+
+    @Context
+    private UriInfo uriInfo;
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response getRoot() {
-        // Implementierung
-        return Response.ok("Root of Customer. Returns array of primary resources").build();
+        String baseUri = uriInfo.getBaseUri().toString();
+
+        Map<String, String> paths = new HashMap<>();
+        paths.put("addresses", baseUri + "addresses");
+        paths.put("customers", baseUri + "customers");
+        paths.put("payments", baseUri + "payments");
+
+
+        return Response.ok(paths).build();
     }
 }
