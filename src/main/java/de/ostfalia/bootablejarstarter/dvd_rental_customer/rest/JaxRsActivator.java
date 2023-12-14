967e90d365380283c597bbb707d5f03078b15472
Mohammad Ghalandari <m.ghalandari@ostfalia.de>
2023-12-14 17:05:42 +0000

    Update 25 files
    - /src/main/resources/META-INF/persistence.xml
    - /src/main/resources/META-INF/urlsLocal.properties
    - /src/main/webapp/WEB-INF/beans.xml
    - /src/main/webapp/WEB-INF/web.xml
    - /src/main/java/de/ostfalia/bootablejarstarter/dvd_rental_customer/control/AddressController.java
    - /src/main/java/de/ostfalia/bootablejarstarter/dvd_rental_customer/control/CityController.java
    - /src/main/java/de/ostfalia/bootablejarstarter/dvd_rental_customer/control/CountryController.java
    - /src/main/java/de/ostfalia/bootablejarstarter/dvd_rental_customer/control/CustomerController.java
    - /src/main/java/de/ostfalia/bootablejarstarter/dvd_rental_customer/control/PaymentController.java
    - /src/main/java/de/ostfalia/bootablejarstarter/dvd_rental_customer/entity/Address.java
    - /src/main/java/de/ostfalia/bootablejarstarter/dvd_rental_customer/entity/City.java
    - /src/main/java/de/ostfalia/bootablejarstarter/dvd_rental_customer/entity/Country.java
    - /src/main/java/de/ostfalia/bootablejarstarter/dvd_rental_customer/entity/Customer.java
    - /src/main/java/de/ostfalia/bootablejarstarter/dvd_rental_customer/entity/Payment.java
    - /src/main/java/de/ostfalia/bootablejarstarter/dvd_rental_customer/rest/address/AddressApi.java
    - /src/main/java/de/ostfalia/bootablejarstarter/dvd_rental_customer/rest/address/AddressJson.java
    - /src/main/java/de/ostfalia/bootablejarstarter/dvd_rental_customer/rest/customer/CustomerApi.java
    - /src/main/java/de/ostfalia/bootablejarstarter/dvd_rental_customer/rest/customer/CustomerJson.java
    - /src/main/java/de/ostfalia/bootablejarstarter/dvd_rental_customer/rest/payment/PaymentApi.java
    - /src/main/java/de/ostfalia/bootablejarstarter/dvd_rental_customer/rest/payment/PaymentJson.java
    - /src/main/java/de/ostfalia/bootablejarstarter/dvd_rental_customer/rest/payment/PaymentReqBody.java
    - /src/main/java/de/ostfalia/bootablejarstarter/dvd_rental_customer/rest/Href.java
    - /src/main/java/de/ostfalia/bootablejarstarter/dvd_rental_customer/rest/JaxRsActivator.java
    - /src/main/java/de/ostfalia/bootablejarstarter/dvd_rental_customer/rest/Resources.java
    - /src/main/java/de/ostfalia/bootablejarstarter/dvd_rental_customer/rest/UrlProperties.java

--- src/main/java/de/ostfalia/bootablejarstarter/dvd_rental_customer/rest/JaxRsActivator.java
+++
@@ -1,9 +0,0 @@
-package de.ostfalia.bootablejarstarter.dvd_rental_customer.rest;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class JaxRsActivator extends Application {
-
-}
