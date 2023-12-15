d55647be7e9de9b9b161faa25dadc93551d3d53b
Mohammad Ghalandari <m.ghalandari@ostfalia.de>
2023-12-15 16:50:57 +0100

    Dockerfile und run-bootable.sh sind ferig

--- run-bootable.sh
+++ run-bootable.sh
@@ -2,7 +2,7 @@
 
 # Umgebungsvariablen definieren
 export POSTGRES_HOST=localhost
-export POSTGRES_PORT=54323
+export POSTGRES_PORT=54323 # muss alle 5432 sein
 export POSTGRES_DB=dvdrentalcustomer
 export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=trust
@@ -15,9 +15,9 @@ java -Djboss.http.port=${HTTP_PORT} \
      -Djboss.bind.address=0.0.0.0 \
      -Djboss.bind.address.management=0.0.0.0 \
      -Djboss.management.http.port=9993 \
-          -Denv.POSTGRES_HOST=${POSTGRES_HOST} \
-          -Denv.POSTGRES_PORT=${POSTGRES_PORT} \
-          -Denv.POSTGRES_DB=${POSTGRES_DB} \
-          -Denv.POSTGRES_USER=${POSTGRES_USER} \
-          -Denv.POSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
+     -Denv.POSTGRES_HOST=${POSTGRES_HOST} \
+     -Denv.POSTGRES_PORT=${POSTGRES_PORT} \
+     -Denv.POSTGRES_DB=${POSTGRES_DB} \
+     -Denv.POSTGRES_USER=${POSTGRES_USER} \
+     -Denv.POSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
      -jar target/dvdrental-customer-bootable.jar
