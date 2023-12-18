afbd1f631e20b8e05ebef753aa146a1477244c88
Mohammad Ghalandari <m.ghalandari@ostfalia.de>
2023-12-18 16:34:50 +0100

    http_port angepasst und mvn clean package eingefügt

--- run-bootable.sh
+++ run-bootable.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-# Umgebungsvariablen definieren
 export POSTGRES_HOST=localhost
-export POSTGRES_PORT=54323 # muss alle 5432 sein
+export POSTGRES_PORT=5432
 export POSTGRES_DB=dvdrentalcustomer
 export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=trust
 
-# Setze den Port für die Anwendung
+
 export HTTP_PORT=8083
 
-# Führe den bootfähigen JAR aus mit den Systemeigenschaften
+mvn clean package
+
+if [ -f "target/dvdrental-customer-bootable.jar" ]; then
 java -Djboss.http.port=${HTTP_PORT} \
      -Djboss.bind.address=0.0.0.0 \
      -Djboss.bind.address.management=0.0.0.0 \
@@ -21,3 +22,7 @@ java -Djboss.http.port=${HTTP_PORT} \
      -Denv.POSTGRES_USER=${POSTGRES_USER} \
      -Denv.POSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
      -jar target/dvdrental-customer-bootable.jar
+else
+     echo "Fehler beim Erstellen der JAR-Datei"
+     exit 1
+fi
